35f67dcede8ebf8ee2b479b0f78bc840efa1e163
61003 <g100m8888@gmail.com>
2021-12-14 21:03:05 +0400

    Lesson 14 (#14)

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -1,12 +1,11 @@
 from basketapp.models import Basket
 
 
-def basket(request):
-    print("context processor basket works")
-    basket = []
 
+def basket(request):
+    basket = []
     if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+        basket = request.user.basket.select_related()
 
     return {
         "basket": basket,
